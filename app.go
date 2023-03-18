d4ff0cd83444655ab29901f078bc02d7770e61e3
Dipokal HHJ <hhj@devent.kr>
2023-03-18 13:43:07 +0900

    feat: add gin
    + gin 라이브러리 설치

--- app.go
+++ app.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Hello world")
+	// http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
+	// 	w.Write([]byte("hello world"))
+	// })
+
+	// http.ListenAndServe(":5100", nil)
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "aaa",
+		})
+	})
+
+	r.Run()
 }
